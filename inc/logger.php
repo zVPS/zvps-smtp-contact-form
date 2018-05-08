0a5d322e28c2362262d49cbc4fd7a78f7967866a
Kevin Andrews <kevin@zvps.uk>
2018-05-08 13:48:25 +0100

    Formatting.

--- inc/logger.php
+++ inc/logger.php
@@ -6,39 +6,38 @@
  */
 class logger
 {
-    
     private static $messages = array('error' => array(), 'success' => array());
-    
+
     public static function addError($message)
     {
         self::$messages['error'][] = $message;
     }
-    
+
     public static function addSuccess($message)
     {
         self::$messages['success'][] = $message;
     }
-    
+
     public static function hasErrors()
     {
         return (count(self::$messages['error']) >= 1) ? true : false;
     }
-    
+
     public static function hasSuccess()
     {
         return (count(self::$messages['success']) >= 1) ? true : false;
     }
-    
+
     public static function hasMessages()
     {
         return (self::hasErrors() || self::hasSuccess()) ? true : false;
     }
-    
+
     public static function getErrors()
     {
         return self::$messages['error'];
     }
-    
+
     public static function getSuccess()
     {
         return self::$messages['success'];
